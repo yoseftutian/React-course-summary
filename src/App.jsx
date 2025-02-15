c33ab3b4bcca437a5319b514db4a5fbd7c49dcfb
yosef-tutian <yoseftutian@gmail.com>
2025-02-15 21:53:58 +0200

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Post from "./components/Post";
 
 function App() {
-  return <Post />;
+  return (
+    <main>
+      <Post author="Maximilian" body="React.js is awesome!" />
+      <Post author="Manuel" body="Check out the full course!" />
+    </main>
+  );
 }
 
 export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,10 @@
-export default function Post() {
-  return <div>Post</div>;
+import classes from "./Post.module.css";
+
+export default function Post(props) {
+  return (
+    <div className={classes.post}>
+      <p className={classes.author}>{props.author}</p>
+      <p className={classes.text}>{props.body}</p>
+    </div>
+  );
 }
