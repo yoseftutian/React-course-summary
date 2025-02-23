e707e0f03a5955c001b1efd1cce152afc5491530
yosef-tutian <yoseftutian@gmail.com>
2025-02-23 19:55:54 +0200

    Add props to NewPost component for better state management

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,22 +1,15 @@
-import { useState } from "react";
 import classes from "./NewPost.module.css";
 
-function NewPost() {
-  const [entereBody, setEntereBody] = useState("");
-  function changeBodyHadler(event) {
-    setEntereBody(event.target.value);
-  }
-
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHadler} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
-      <p>{entereBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 export default function PostsList() {
+  const [entereBody, setEntereBody] = useState("");
+  const [enteredAuyhor, setEnteredAuyhor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEntereBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuyhor(event.target.value);
+  }
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="Maximilian" body="React.js is awesome!" />
+        <Post author={enteredAuyhor} body={entereBody} />
         <Post author="Manuel" body="Check out the full course!" />
       </ul>
     </>
