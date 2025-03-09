8b30484954875de79d320a7057dc7375b67f329e
yosef-tutian <yoseftutian@gmail.com>
2025-03-09 19:52:33 +0200

    add "Add Post" button

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -22,6 +22,7 @@ export default function PostsList({ isposting, onStopPosting }) {
           <NewPost
             onBodyChange={bodyChangeHandler}
             onAuthorChange={authorChangeHandler}
+            onCancel={onStopPosting}
           />
         </Modal>
       )}
