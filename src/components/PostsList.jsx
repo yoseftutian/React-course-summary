86cb6fc7ade6dd1969a7f1be658c2bace39b11ad
yosef-tutian <yoseftutian@gmail.com>
2025-03-28 15:10:36 +0300

    Show loading message while fetching posts

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,12 +6,15 @@ import Modal from "./Modal";
 
 export default function PostsList({ isposting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:8080/posts");
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false);
     }
     fetchPosts();
   }, []);
@@ -35,17 +38,22 @@ export default function PostsList({ isposting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>No posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>Loading...</h2>
         </div>
       )}
     </>
